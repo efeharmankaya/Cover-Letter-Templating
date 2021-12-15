579c159858c961d3e3b6f27da456deb4eab8d8c9
Efe Harmankaya <eharmankaya.18@gmail.com>
2021-12-15 14:21:49 -0500

    refactoring + readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Cover Letter Templating
- 
-## Setup
+Cover Letter Templating aids in automating the repetitive task of copy and pasting in specific job information into a prewritten cover letter template. With this repo you can easily setup your cover letter template to accept parameterized sections and create an automated pdf file based on your template image.
+
+# Setup
 Clone repo to your local environment
 ```bash
 git clone https://github.com/efeharmankaya/Cover-Letter-Templating.git
@@ -9,10 +10,14 @@ Install pip package for PDF writing
 ```bash
 pip3 install fpdf
 ```
+
+# Usage
+
 Follow `TODO:` tags in main.py
 
-1) Replace lorem ipsum cover letter template with person letter template following the template convention
-2) Set params based on job posting information
+0. Before beginning you must have a prewritten cover letter template 
+1. After cloning the repo, replace lorem ipsum cover letter template with your letter template following the template convention from the given example (ie. any text that should be variable should be )
+2) Set parameters based on job posting information
 
 ## Troubleshooting
 [PyFPDF docs](https://pyfpdf.readthedocs.io/en/latest/index.html)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 from fpdf import FPDF
+
 A4_WIDTH = 210
 A4_HEIGHT = 297
 
@@ -12,18 +13,18 @@ def cover_letter(params):
     '''
         1) TODO: format TEMPLATE cover letter below to own personal experience
     '''
-    TEMPLATE = f"""{params["date"]}
-{params["recruiter_name"]}
-{params["position"]}
-{params["company"]}
-{params["company_address"]}
-{params["location_params"]}
-Subject: {params["position"]} with {params["company"]} (Job ID: {params["id"]})
-Dear {params["salutation"] + ". " if params["salutation"] else ""}{params["recruiter_name"]},
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in mauris lectus. Curabitur ut facilisis est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed velit risus, pulvinar et placerat id {"an" if vowel_start(params["position"]) else "a"} {params["position"]}, duis eleifend tincidunt sapien. Mauris laoreet purus ante {params["company"]} nunc malesuada id justo vitae accumsan. Nam pharetra orci et pretium elementum {params["company"]}'s ligula ipsum egestas ex. Duis vel varius odio {params["company"]}'s pharetra vitae dolor vel, venenatis gravida neque {params["mission_statement_keywords"]}.
+    TEMPLATE = f"""{params.get("date", "MISSING_PARAMETER (date)")}
+{params.get("recruiter_name", "MISSING_PARAMETER (recruiter_name)")}
+{params.get("position", "MISSING_PARAMETER (position)")}
+{params.get("company", "MISSING_PARAMETER (company)")}
+{params.get("company_address", "MISSING_PARAMETER (company_address)")}
+{params.get("location_params", "MISSING_PARAMETER (location_params)")}
+Subject: {params.get("position", "MISSING_PARAMETER (position)")} with {params.get("company", "MISSING_PARAMETER (company)")} (Job ID: {params.get("id", "MISSING_PARAMETER (id)")})
+Dear {params.get("salutation", "MISSING_PARAMETER (salutation)") + ". " if params.get("salutation", "MISSING_PARAMETER (salutation)") else ""}{params.get("recruiter_name", "MISSING_PARAMETER (recruiter_name)")},
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in mauris lectus. Curabitur ut facilisis est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed velit risus, pulvinar et placerat id {"an" if vowel_start(params.get("position", "MISSING_PARAMETER (position)")) else "a"} {params.get("position", "MISSING_PARAMETER (position)")}, duis eleifend tincidunt sapien. Mauris laoreet purus ante {params.get("company", "MISSING_PARAMETER (company)")} nunc malesuada id justo vitae accumsan. Nam pharetra orci et pretium elementum {params.get("company", "MISSING_PARAMETER (company)")}'s ligula ipsum egestas ex. Duis vel varius odio {params.get("company", "MISSING_PARAMETER (company)")}'s pharetra vitae dolor vel, venenatis gravida neque {params.get("mission_statement_keywords", "MISSING_PARAMETER (mission_statement_keywords)")}.
 Nullam sodales ex sit amet vehicula tincidunt. Quisque in aliquet velit, at aliquet diam. Cras porttitor lacinia ipsum id bibendum. Nulla luctus massa a pulvinar molestie. Fusce aliquet vel velit et iaculis. Cras at arcu venenatis, feugiat est ut, ornare ligula. Duis vel ullamcorper enim. Etiam mattis tellus in finibus placerat. Etiam quis convallis metus.
 Vestibulum facilisis magna a maximus dapibus. Curabitur sodales massa in justo convallis, eu gravida sapien fringilla. Quisque imperdiet, arcu vel laoreet bibendum, diam nisi commodo leo, eget elementum sem metus in tellus. Integer egestas accumsan neque id tincidunt. Vestibulum non orci id purus ornare luctus.
-Donec viverra mi sit amet ipsum lobortis finibus {"an" if vowel_start(params["position"]) else "a"} {params["position"]}.
+Donec viverra mi sit amet ipsum lobortis finibus {"an" if vowel_start(params.get("position", "MISSING_PARAMETER (position)")) else "a"} {params.get("position", "MISSING_PARAMETER (position)")}.
 
 Aenean vel finibus ipsum,
 Sed a urna at tortor,
@@ -37,23 +38,8 @@ class PDF(FPDF):
         self.image('coop_cover_letter_template.png', 0, 0, A4_WIDTH, A4_HEIGHT - 10)
         self.ln(20)
 
-def main():
-    '''
-        2) TODO: Change values in 'params' below based on posting data
-    '''
-    # Setup cover letter parameters
-    params = {
-        'date' : str(datetime.date.today()),
-        'id' : 'POST_ID',
-        'recruiter_name' : 'RECRUITER NAME',
-        'position' : 'POSITION',
-        'company' : 'COMPANY',
-        'company_address' : 'ADDRESS',
-        'location_params' : 'LOCATION',
-        'salutation' : 'Mr',
-        'mission_statement_keywords' : 'COMPANY SPECIFIC'
-    }
 
+def generate_cover_letter(params):
     # Setup PDF environment
     pdf = PDF()
     pdf.alias_nb_pages()
@@ -67,6 +53,43 @@ def main():
         pdf.ln(2)
     pdf.output(f'cover_letters/cover_letter_{params["id"]}_{params["company"].split(" ")[0]}.pdf', 'F')
 
+
+def main():
+    '''
+        2) TODO: setup parameters based on the job posting information following the convention below
+        Note: should you want to generate multiple cover letters, simply add another dictionary based on the template below into the params list (ie. where the ... is)
+        params = [
+            {
+                'date' : str(datetime.date.today()),
+                'id' : 'POST_ID',
+                'recruiter_name' : 'RECRUITER NAME',
+                'position' : 'POSITION',
+                'company' : 'COMPANY',
+                'company_address' : 'ADDRESS',
+                'location_params' : 'LOCATION',
+                'salutation' : 'SALUTATION',
+                'mission_statement_keywords' : 'COMPANY SPECIFIC'
+            },
+            ...
+        ]
+    '''
+    params = [
+        {
+            'date' : str(datetime.date.today()),
+            'id' : 'POST_ID',
+            'recruiter_name' : 'RECRUITER NAME',
+            'position' : 'POSITION',
+            'company' : 'COMPANY',
+            'company_address' : 'ADDRESS',
+            'location_params' : 'LOCATION',
+            'salutation' : 'SALUTATION',
+            'mission_statement_keywords' : 'COMPANY SPECIFIC'
+        }
+    ]
+    
+    for param in params:
+        generate_cover_letter(param)
+    
 
 if __name__ == '__main__':
     main()
